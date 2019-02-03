5300001b243603082b26303f6bdbd2dbcd4a1eff
ciricc <facevk.02@mail.ru>
2019-02-03 19:08:08 +0300

    2.1.12

--- src/easyvk.js
+++ src/easyvk.js
@@ -663,7 +663,6 @@ module.exports.class = {
   AudioItem: 'AudioItem'
 }
 
-
 module.exports.version = '2.2.12'
 module.exports.callbackAPI = new EasyVKCallbackAPI({})
 module.exports.streamingAPI = new EasyVKStreamingAPI({})
