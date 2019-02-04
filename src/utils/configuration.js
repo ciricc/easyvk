1a0ee87796c591566e50bbf37b1551237be6bced
ciricc <facevk.02@mail.ru>
2019-02-04 17:41:16 +0300

    Added support for user agent in vk.call() method

--- src/utils/configuration.js
+++ src/utils/configuration.js
@@ -43,4 +43,6 @@ configuration.HTTP_CLIENT = {
   COOKIE_PATH: path.join(__dirname, '.evk-cookies.json')
 }
 
+configuration.DEFAULT_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.81 Safari/537.36'
+
 module.exports = configuration
--- src/utils/staticMethods.js
+++ src/utils/staticMethods.js
@@ -7,200 +7,219 @@ const VKResponse = require('./VKResponse.js')
 const configuration = require('./configuration.js')
 const VKResponseError = require('./VKResponseError.js')
 
-module.exports.isString = function (n) {
-  return Object.prototype.toString.call(n) === '[object String]'
-}
+class StaticMethods {
+  constructor (settings = {}) {
+    this.settings = settings
+  }
 
-module.exports.isObject = function (n) {
-  return Object.prototype.toString.call(n) === '[object Object]'
-}
+  static isString (n) {
+    return Object.prototype.toString.call(n) === '[object String]'
+  }
 
-module.exports.checkJSONErrors = function (vkr, reject) {
-  let self = this
+  static isObject (n) {
+    return Object.prototype.toString.call(n) === '[object Object]'
+  }
 
-  try {
-    vkr = JSON.parse(vkr)
+  static checkJSONErrors (vkr, reject) {
+    let self = this
 
-    let err = self.checkErrors(vkr)
+    try {
+      vkr = JSON.parse(vkr)
 
-    if (err) {
-      if (typeof err === 'string') {
-        // new error
-        err = new Error(err)
+      let err = self.checkErrors(vkr)
+
+      if (err) {
+        if (typeof err === 'string') {
+          // new error
+          err = new Error(err)
+        }
+
+        reject(err)
+        return false
       }
 
-      reject(err)
-      return false
+      return VKResponse(self, vkr)
+    } catch (e) {
+      if (e.name === 'SyntaxError') {
+        let err = new Error('Server sent not a json object (' + vkr + ')')
+
+        return reject(err)
+      }
+
+      return reject(new Error(e))
     }
-
-    return VKResponse(self, vkr)
-  } catch (e) {
-    if (e.name === 'SyntaxError') {
-      let err = new Error('Server sent not a json object (' + vkr + ')')
-
-      return reject(err)
-    }
-
-    return reject(new Error(e))
   }
-}
 
-module.exports.urlencode = function (object = {}) {
-  let self = this
+  static urlencode (object = {}) {
+    let self = this
 
-  return Object.keys(object)
-    .map(prop =>
-      prop + '=' + (
-        (self.isObject(object[prop]))
-          ? (encodeURIComponent(JSON.stringify(object[prop]))) : encodeURIComponent(object[prop])
+    return Object.keys(object)
+      .map(prop =>
+        prop + '=' + (
+          (self.isObject(object[prop]))
+            ? (encodeURIComponent(JSON.stringify(object[prop]))) : encodeURIComponent(object[prop])
+        )
       )
-    )
-    .join('&')
-}
+      .join('&')
+  }
 
-module.exports.checkErrors = function (vkr) {
-  try {
-    if (vkr.error) {
-      if (vkr.error === 'need_captcha' || vkr.error.error_code === 14) {
-        return JSON.stringify(vkr)
-      } else if (vkr.error === 'need_validation') {
-        if (vkr.ban_info) {
-          return vkr.error_description
-        } else {
-          let type = 'sms'
+  static checkErrors (vkr) {
+    try {
+      if (vkr.error) {
+        if (vkr.error === 'need_captcha' || vkr.error.error_code === 14) {
+          return JSON.stringify(vkr)
+        } else if (vkr.error === 'need_validation') {
+          if (vkr.ban_info) {
+            return vkr.error_description
+          } else {
+            let type = 'sms'
 
-          if (vkr.validation_type.match('app')) {
-            type = 'app'
+            if (vkr.validation_type.match('app')) {
+              type = 'app'
+            }
+
+            return {
+              error: `Please, enter your ${type} code in code parameter!`,
+              error_code: vkr.error,
+              validation_type: vkr.validation_type,
+              validation_sid: vkr.validation_sid,
+              redirect_uri: vkr.redirect_uri
+            }
           }
-
+        } else if (vkr.error.error_code === 17) {
           return {
-            error: `Please, enter your ${type} code in code parameter!`,
-            error_code: vkr.error,
-            validation_type: vkr.validation_type,
-            validation_sid: vkr.validation_sid,
-            redirect_uri: vkr.redirect_uri
+            redirect_uri: vkr.error.redirect_uri,
+            error: vkr.error.error_msg,
+            error_code: vkr.error.error_code
           }
         }
-      } else if (vkr.error.error_code === 17) {
-        return {
-          redirect_uri: vkr.error.redirect_uri,
-          error: vkr.error.error_msg,
-          error_code: vkr.error.error_code
+
+        if (vkr.error.error_msg) {
+          return new VKResponseError(vkr.error.error_msg, vkr.error.error_code, vkr.error.request_params)
+        } else if (vkr.error.message) {
+          return new VKResponseError(vkr.error.message, vkr.error.code, vkr.error.params)
+        } else {
+          return new VKResponseError(vkr.error_description, vkr.error)
+        }
+      }
+    } catch (e) {
+      return e
+    }
+  }
+
+  static async call (methodName, data = {}, methodType = 'get', debuggerIS = null, Agent, settings = {}) {
+    let self = this
+
+    return new Promise((resolve, reject) => {
+      let req
+      let methodTypeLower = methodType.toString().toLocaleLowerCase()
+
+      if (methodTypeLower !== 'post') {
+        methodType = 'get'
+      }
+
+      if (!methodName) {
+        return reject(new Error('Put method name in your call request!'))
+      }
+
+      if (!data.v) {
+        data.v = configuration.api_v
+      }
+
+      if (!settings.userAgent) {
+        settings.userAgent = configuration.DEFAULT_USER_AGENT
+      }
+
+      let callParams = {
+        url: configuration.BASE_CALL_URL + methodName
+      }
+
+      let data2 = JSON.parse(JSON.stringify(data))
+
+      if (methodType === 'post') {
+        // prepare post request
+        callParams.agent = Agent
+
+        callParams.form = data
+        callParams.headers = {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'User-agent': settings.userAgent
+        }
+
+        // Nice request recommendtion
+        for (let i in callParams.form) {
+          if (self.isObject(callParams.form[i])) { callParams.form[i] = JSON.stringify(callParams.form[i]) }
+        }
+
+        req = request.post
+      }
+
+      if (debuggerIS) {
+        try {
+          debuggerIS.push('fullRequest', callParams)
+        } catch (e) {
+          return reject(new Error('No have a complite debuggerIS'))
         }
       }
 
-      if (vkr.error.error_msg) {
-        return new VKResponseError(vkr.error.error_msg, vkr.error.error_code, vkr.error.request_params)
-      } else if (vkr.error.message) {
-        return new VKResponseError(vkr.error.message, vkr.error.code, vkr.error.params)
+      if (methodType === 'get') {
+        data = querystring.stringify(data)
+
+        let options = {
+          host: 'api.' + configuration.BASE_DOMAIN,
+          agent: Agent,
+          path: '/method/' + methodName + '?' + data,
+          headers: {
+            'User-Agent': settings.userAgent
+          }
+        }
+        return https.get(options, (res) => {
+          let vkr = ''
+          res.on('data', (chu) => {
+            vkr += chu
+          })
+          res.on('end', () => { return parseResponse(vkr) })
+        }).on('error', (e) => {
+          try {
+            reject(e)
+          } catch (err) {
+            throw e
+          }
+        })
       } else {
-        return new VKResponseError(vkr.error_description, vkr.error)
+        req(callParams, (err, res) => {
+          if (err) { return reject(new Error(err)) }
+          return parseResponse(res.body)
+        })
       }
-    }
-  } catch (e) {
-    return e
+
+      async function parseResponse (vkr) {
+        if (vkr) {
+          if (debuggerIS) {
+            try {
+              debuggerIS.push('response', vkr)
+            } catch (e) {}
+          }
+
+          let json = self.checkJSONErrors(vkr, reject)
+
+          if (json) {
+            json.queryData = data2
+
+            return resolve(json)
+          } else {
+            return reject(new Error("JSON is not valid... oor i don't know"))
+          }
+        } else {
+          return reject(new Error(`Empty response ${vkr}`))
+        }
+      }
+    })
+  }
+
+  async call () {
+    return StaticMethods.call(...arguments, this.settings)
   }
 }
 
-module.exports.call = async function call (methodName, data = {}, methodType = 'get', debuggerIS = null, Agent) {
-  let self = this
-
-  return new Promise((resolve, reject) => {
-    let req
-    let methodTypeLower = methodType.toString().toLocaleLowerCase()
-
-    if (methodTypeLower !== 'post') {
-      methodType = 'get'
-    }
-
-    if (!methodName) {
-      return reject(new Error('Put method name in your call request!'))
-    }
-
-    if (!data.v) {
-      data.v = configuration.api_v
-    }
-
-    let callParams = {
-      url: configuration.BASE_CALL_URL + methodName
-    }
-
-    let data2 = JSON.parse(JSON.stringify(data))
-
-    if (methodType === 'post') {
-      // prepare post request
-      callParams.agent = Agent
-
-      callParams.form = data
-      callParams.headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-
-      // Nice request recommendtion
-      for (let i in callParams.form) {
-        if (self.isObject(callParams.form[i])) { callParams.form[i] = JSON.stringify(callParams.form[i]) }
-      }
-
-      req = request.post
-    }
-
-    if (debuggerIS) {
-      try {
-        debuggerIS.push('fullRequest', callParams)
-      } catch (e) {
-        return reject(new Error('No have a complite debuggerIS'))
-      }
-    }
-
-    if (methodType === 'get') {
-      data = querystring.stringify(data)
-
-      let options = {
-        host: 'api.' + configuration.BASE_DOMAIN,
-        agent: Agent,
-        path: '/method/' + methodName + '?' + data
-      }
-
-      return https.get(options, (res) => {
-        let vkr = ''
-        res.on('data', (chu) => {
-          vkr += chu
-        })
-        res.on('end', () => { return parseResponse(vkr) })
-      }).on('error', (e) => {
-        try {
-          reject(e)
-        } catch (err) {
-          throw e
-        }
-      })
-    } else {
-      req(callParams, (err, res) => {
-        if (err) { return reject(new Error(err)) }
-        return parseResponse(res.body)
-      })
-    }
-
-    async function parseResponse (vkr) {
-      if (vkr) {
-        if (debuggerIS) {
-          try {
-            debuggerIS.push('response', vkr)
-          } catch (e) {}
-        }
-
-        let json = self.checkJSONErrors(vkr, reject)
-
-        if (json) {
-          json.queryData = data2
-
-          return resolve(json)
-        } else {
-          return reject(new Error("JSON is not valid... oor i don't know"))
-        }
-      } else {
-        return reject(new Error(`Empty response ${vkr}`))
-      }
-    }
-  })
-}
+module.exports = StaticMethods
