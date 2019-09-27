be594b11e51f228ff2b081844dde002d123e96db
ciricc <facevk.02@mail.ru>
2019-09-28 04:23:59 +0500

    brush

--- rollup.config.js
+++ rollup.config.js
@@ -8,32 +8,32 @@ const sourcePath = path.join(__dirname, 'src', 'index.ts');
 const libraryPath = path.join(__dirname, 'lib', 'index.js');
 
 export default {
-    input: 'src/index.ts',
-    output: [
-        {
-            file: 'lib/index.js',
-            format: 'cjs',
-            sourcemap: true
-        }
-    ],
-    plugins: [
-        resolve(),
-        typescript({
-            rollupCommonJSResolveHack: true,
-            useTsconfigDeclarationDir: false,
-            target: "es6",
-            tsconfigOverride: {
-                outDir: 'lib',
-                rootDir: 'src',
-                include: ['src'],
-                declaration: true,
-                strict: true
-            }
-        }),
-        commonjs(),
-    ],
-    watch: {
-        chokidar: false,
-        exclude: ["lib"]
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'lib/index.js',
+      format: 'cjs',
+      sourcemap: true
     }
+  ],
+  plugins: [
+    resolve(),
+    typescript({
+      rollupCommonJSResolveHack: true,
+      useTsconfigDeclarationDir: false,
+      target: "es6",
+      tsconfigOverride: {
+        outDir: 'lib',
+        rootDir: 'src',
+        include: ['src'],
+        declaration: true,
+        strict: true
+      }
+    }),
+    commonjs(),
+  ],
+  watch: {
+    chokidar: false,
+    exclude: ["lib"]
+  }
 }
