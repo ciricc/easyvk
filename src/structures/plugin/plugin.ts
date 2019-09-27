a1756e691298ddc777d35511a21fad241ae0c41e
ciricc <facevk.02@mail.ru>
2019-09-27 23:36:29 +0500

    Normal add plugin

--- src/structures/plugin/plugin.ts
+++ src/structures/plugin/plugin.ts
@@ -2,9 +2,14 @@ import VK from "../../vk";
 
 class Plugin {
     public vk:VK;
+    /** Plugin name (unique) */
     public name = "defaultPlugin";
+    /** Plugin options */
     public options = {};
+    /** Plugin names which need for usage and install this plugin */
     public requirements = [];
+    /** Plugin name which after this plugin will be installed */
+    public setupAfter:string;
 
     constructor (vk, options) {
         this.vk = vk;
@@ -18,6 +23,5 @@ class Plugin {
 
     }
 }
-
 
 export default Plugin;
